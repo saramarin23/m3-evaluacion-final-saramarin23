523a77bd557b7b6b370a75360729f8e09c61b5ee
saramarin23 <saramarin.econ@gmail.com>
2019-09-23 16:09:23 +0200

    add searcher input

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      data: [],
+      query: ""
     };
+    this.inputSearch = this.inputSearch.bind(this);
   }
 
   componentDidMount() {
@@ -19,32 +21,46 @@ class App extends React.Component {
     fetch(URL)
       .then(response => response.json())
       .then(data => {
-        console.log(data.results);
         this.setState({
           data: data.results
         });
       });
   }
 
+  inputSearch(e) {
+    const value = e.currentTarget.value;
+    console.log(value);
+    this.setState({ query: value });
+  }
+
   render() {
-    const { data } = this.state;
+    const { data, query } = this.state;
     return (
       <div className="App">
+        <img
+          className="home-logo"
+          src="./official-logo.png"
+          alt="Evaluación final"
+        />
+        <input type="search" onChange={this.inputSearch} />
         <ul className="App__List">
-          {data.map(character => {
-            console.log(character);
-            return (
-              <li className="App__List--c" key={character.id}>
-                <img
-                  className="App__List--c-photo"
-                  src={character.image}
-                  alt={character.name}
-                />
-                <h2 className="App__List--c-name">{character.name}</h2>
-                <p className="App__List--c-specie">{character.species}</p>
-              </li>
-            );
-          })}
+          {data
+            .filter(myCharacter =>
+              myCharacter.name.toLowerCase().includes(query.toLowerCase())
+            )
+            .map(character => {
+              return (
+                <li className="App__List--c" key={character.id}>
+                  <img
+                    className="App__List--c-photo"
+                    src={character.image}
+                    alt={character.name}
+                  />
+                  <h2 className="App__List--c-name">{character.name}</h2>
+                  <p className="App__List--c-specie">{character.species}</p>
+                </li>
+              );
+            })}
         </ul>
       </div>
     );
