2ddf6e076341c01a1a6b8984f896ceb7812b205f
saramarin23 <saramarin.econ@gmail.com>
2019-09-23 15:56:56 +0200

    add docs folder, ready to deploy

---
+++ docs/precache-manifest.54d79827d6d6a67ebce93c9ca72cf544.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e94f938e0b46bb18b6840875cc4a10",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b51383b3f88a0457512f",
+    "url": "./static/css/main.369ad437.chunk.css"
+  },
+  {
+    "revision": "5ca5cd0b43b22a6a7feb",
+    "url": "./static/js/2.073fe3ca.chunk.js"
+  },
+  {
+    "revision": "b51383b3f88a0457512f",
+    "url": "./static/js/main.38d7c692.chunk.js"
+  },
+  {
+    "revision": "6bec1b601a50888ea15c",
+    "url": "./static/js/runtime-main.49bc8632.js"
+  }
+]);
